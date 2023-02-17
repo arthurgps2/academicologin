f28fd1019f3267623b08ab9d1382b7c45b9b518f
arthurgps2 <arthurgps2@gmail.com>
2023-02-16 22:17:12 -0300

    Classe User atualizada e criada classe Role

--- academico/src/main/java/com/fieb/academico/model/User.java
+++ academico/src/main/java/com/fieb/academico/model/User.java
@@ -1,5 +1,24 @@
 package com.fieb.academico.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
 	
+	// Class User
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long d;
+	private String firstName;
+	private String lastName;
+	
+	private String email;
+	private String password;
 }
